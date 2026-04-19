a9201a6d35fdb82ac6fed243bd8d9a282ec71fb1
RSetor77 <vasyabikov789@gmail.com>
2026-04-19 16:39:37 +0300

    Исправил баг ненахождения интерфейса

--- Models/OutputValueType.cs
+++ Models/OutputValueType.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Jint;
+using Jint.Runtime;
+using MockDataGenerator.Interfaces;
+using MockDataGenerator.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel;
@@ -15,5 +19,52 @@ namespace MockDataGenerator.Models
         public required ValueTypes Type { get; set; }
         public required GenerationTypes GenerationType { get; set; }
         public required Dictionary<string, object> Data { get; set; }
+        [JsonIgnore]
+        private Func<string>? _compiledFormula;
+        public void Prepare()
+        {
+            if (GenerationType == GenerationTypes.Formula && Data.TryGetValue("Formula", out var formulaRaw))
+            {
+                string formula = formulaRaw.ToString()!;
+                if (!formula.Contains("return") && !formula.Contains(';'))
+                    formula = "return " + formula;
+                try
+                {
+                    var compiledScript = Engine.PrepareScript(formula);
+                    _compiledFormula = () =>
+                    {
+                        try
+                        {
+                            var del = FormulaService.engine.Evaluate(compiledScript).ToString();
+                            return del;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            return "NULL";
+                        }
+
+                    };
+                }
+                catch (JavaScriptException jsEx)
+                {
+                    //Сообщить об ошибке в коде
+                    Console.WriteLine(jsEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    //Сообщить об ошибке
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+        public string GetFormulaOutput()
+        {
+            if (GenerationType == GenerationTypes.Formula && _compiledFormula != null)
+                return _compiledFormula.Invoke();
+            else
+                return string.Empty;
+        }
     }
 }
